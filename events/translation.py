86270fd0f2e2523171b5250b756b050c425db41d
Perfectionist25 <ali20032508@gmail.com>
2025-11-19 18:06:13 +0500

    lacalize-resources-and-part-of-accounts

--- events/translation.py
+++ events/translation.py
@@ -1,8 +1,19 @@
 from modeltranslation.translator import translator, TranslationOptions
-from .models import Event, EventCategory, EventSpeaker, EventSession
+from .models import (
+    EventCategory,
+    Event,
+    EventRegistration,
+    EventSpeaker,
+    EventSession,
+    EventComment,
+    EventPhoto,
+    EventRating,
+)
+
 
 class EventCategoryTranslationOptions(TranslationOptions):
     fields = ('name', 'description')
+
 
 class EventTranslationOptions(TranslationOptions):
     fields = (
@@ -12,16 +23,39 @@ class EventTranslationOptions(TranslationOptions):
         'location',
         'venue',
         'address',
-        'tags'
+        'tags',
     )
+
+
+class EventRegistrationTranslationOptions(TranslationOptions):
+    fields = ('dietary_restrictions', 'special_requirements', 'comments')
+
 
 class EventSpeakerTranslationOptions(TranslationOptions):
     fields = ('name', 'title', 'organization', 'bio')
 
+
 class EventSessionTranslationOptions(TranslationOptions):
     fields = ('title', 'description', 'location')
 
+
+class EventCommentTranslationOptions(TranslationOptions):
+    fields = ('comment',)
+
+
+class EventPhotoTranslationOptions(TranslationOptions):
+    fields = ('caption',)
+
+
+class EventRatingTranslationOptions(TranslationOptions):
+    fields = ('comment',)
+
+
 translator.register(EventCategory, EventCategoryTranslationOptions)
 translator.register(Event, EventTranslationOptions)
+translator.register(EventRegistration, EventRegistrationTranslationOptions)
 translator.register(EventSpeaker, EventSpeakerTranslationOptions)
 translator.register(EventSession, EventSessionTranslationOptions)
+translator.register(EventComment, EventCommentTranslationOptions)
+translator.register(EventPhoto, EventPhotoTranslationOptions)
+translator.register(EventRating, EventRatingTranslationOptions)
