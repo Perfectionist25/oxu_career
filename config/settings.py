886df58012d1cce70531aef01e8f6c155d7542e1
perfy <perfy@perfy.168.1.4>
2026-02-12 10:33:11 +0500

    hide db data

--- config/settings.py
+++ config/settings.py
@@ -189,11 +189,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "myuser"),
-        "USER": os.getenv("DB_USER", "mydb"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "oxu_career_2026"),
-        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT")
     }
 }
 
