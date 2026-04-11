7f31d84cf369e3d02717ad7e81dc93b7251fc2e7
Perfectionist25 <ali20032508@gmail.com>
2026-04-11 17:20:58 +0500

    test2

--- accounts/forms.py
+++ accounts/forms.py
@@ -499,7 +499,6 @@ class CompanyDocumentForm(forms.ModelForm):
             'file': forms.FileInput(attrs={'class': 'form-control'}),
         }
 
-# jkladfjlaskdjf
 class StudentProfileForm(forms.ModelForm):
     """Форма профиля студента/выпускника"""
     class Meta:
